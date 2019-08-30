7cc48c6ea33fb224c0da4b48ed8d0e1cf2f36057
Tusharsaxena3112 <tushar.saxena_cs18@gla.ac.in>
2019-08-30 23:04:18 +0530

    adding Question4.java complete

--- src/question4/Question4.java
+++ src/question4/Question4.java
@@ -1,5 +1,15 @@
 package question4;
 
 public class Question4 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 9; i++) {
+            for (int j = 1; j <= 9; j++) {
+                if (j <= i) {
+                    System.out.printf("%d  ", i);
+                }
+            }
+            System.out.println();
+        }
+    }
 
 }
