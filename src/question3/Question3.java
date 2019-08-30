e0a63b2dabd687a3417c804e68d7f34c1e199c39
Tusharsaxena3112 <tushar.saxena_cs18@gla.ac.in>
2019-08-30 22:56:56 +0530

    changing var name

--- src/question3/Question3.java
+++ src/question3/Question3.java
@@ -5,8 +5,8 @@ import java.util.Scanner;
 public class Question3 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        double farhenheit = input.nextDouble();
-        double celcius = (farhenheit - 32) / 1.8;
-        System.out.println(celcius);
+        double fahrenheit = input.nextDouble();
+        double celsius = (fahrenheit - 32) / 1.8;
+        System.out.println(celsius);
     }
 }
